b31597d1fb00253e3464a233c0a7b5c12e5d5ba9
khushboo3134 <khushbook3132k3@gmail.com>
2026-01-17 23:01:55 +0530

    Delete SchoolManagementSystem directory

--- SchoolManagementSystem/util/InputUtil.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-import java.util.Scanner;
-
-public class InputUtil {
-    public static Scanner sc = new Scanner(System.in);  // static use kr rhe h taki bina object bnaye hr jagah use kr ske. ab khi bhi use krna hoga toh InputUtil.sc.nextInt() likh skti hoon
-}
